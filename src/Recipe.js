72bad86efb8eb48a83e5a9aace0935490d2efde3
Sam Chillcott <samchillcott@gmail.com>
2020-05-07 12:21:40 +0100

    12.21pm

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,14 +4,16 @@ import style from "./recipe.module.css";
 const Recipe = ({ title, calories, image, ingredients }) => {
 	return (
 		<div className={style.recipe}>
-			<h1>{title}</h1>
-			<ol>
-				{ingredients.map((ingredient) => (
-					<li>{ingredient.text}</li>
-				))}
-			</ol>
-			<p>{{calories} = Math.round(calories)} Calories</p>
+			<h1 className={style.title}>{title}</h1>
 			<img className={style.image} src={image} alt="" />
+			<p>{({ calories } = Math.round(calories))} Calories</p>
+			<div className={style.wrapper}>
+				<ol className={style.ingredient}>
+					{ingredients.map((ingredient) => (
+						<li>{ingredient.text}</li>
+					))}
+				</ol>
+			</div>
 		</div>
 	);
 };
